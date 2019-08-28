f667b359b5d8bb34bcda401596ca028989474c84
Ryan Ansberry <ansman92@gmail.com>
2019-08-28 15:18:03 -0700

    added final model functions

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,14 +2,45 @@ const db = require('../data/db-config.js');
 
 module.exports = {
     find,
-    findById
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
 };
 
 function find() {
     return db('schemes');
-  }
+}
   
-  function findById(id) {
+function findById(id) {
     return db('schemes').where({ id }).first();
-  }
+}
+
+function findSteps(scheme_id) {
+    return db('schemes as sc')
+    .join('steps as st', 'st.scheme_id', '=', 'sc.id')
+    .select('st.id', 'st.step_number', 'st.instructions','sc.id')
+    .orderBy('st.step_number')
+    .where({ scheme_id })
+}
+
+function add(scheme) {
+    return db('schemes').insert(scheme)
+    .then(ids => {
+        return findById(ids[0]);
+    });
+}
+
+function update(changes, id) {
+    return db('schemes')
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+    return db('schemes')
+    .where({ id })
+    .del();
+}
   
